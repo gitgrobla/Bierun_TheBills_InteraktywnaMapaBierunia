ab3bad6a7de5c7016c1b09072d97f2a85628adb3
Kacper Grobelny <scottnax.senic@gmail.com>
2023-11-25 00:19:32 +0100

    model viewer workin

--- bierunapp/src/App.tsx
+++ bierunapp/src/App.tsx
@@ -5,22 +5,29 @@ import "./App.css";
 import * as THREE from "three";
 import { createRoot } from "react-dom/client";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements, useThree } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, ThreeElements, Camera } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera, useCamera } from "@react-three/drei";
 
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { useLoader } from "@react-three/fiber";
 
 import Box from "./Box";
 
 function App() {
-  const obj = useLoader(OBJLoader, "/Bierun003.obj");
+  // const obj = useLoader(OBJLoader, "/Bierun003.obj");
+  //  const obj = useLoader(FBXLoader, "/Bierun001.fbx");
+  // const obj = useLoader(FBXLoader, "/house.fbx");
+  const obj = useLoader(FBXLoader, "/house.fbx");
 
   return (
     <div className="main">
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+      
+      <Canvas
+        shadows
+      >
+        <PerspectiveCamera makeDefault position={[0, 0, 500]} />
+        <ambientLight intensity={1} />
         <primitive object={obj} />
         <OrbitControls />
       </Canvas>

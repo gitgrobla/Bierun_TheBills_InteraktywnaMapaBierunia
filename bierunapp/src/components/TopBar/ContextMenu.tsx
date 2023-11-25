0ddd982f89edcd83436c6dfb885cb42a403c7cb9
Wiktor <266597@student.pwr.edu.pl>
2023-11-25 20:32:40 +0100

    added hide button

--- bierunapp/src/components/TopBar/ContextMenu.tsx
+++ bierunapp/src/components/TopBar/ContextMenu.tsx
@@ -10,12 +10,24 @@ function ContextMenu({}: Props) {
   return (
     <>
     <div className={`${styles.contextMenu} ${!visible?styles.hidden:""}`}>
-      <div>Dodaj inwestycję</div>
-      <div>Dodaj propozycje</div>
-      <div>Obserwowane inwestycje</div>
-      <div>Obserwowane propozycje</div>
+      <div>
+        <img src="/svg/location_city_FILL1_wght400_GRAD0_opsz24.svg" alt="plus" />
+        Dodaj inwestycję</div>
+      <div>
+      <img src="/svg/forum_FILL1_wght400_GRAD0_opsz24.svg" alt="plus" />
+        Dodaj propozycje</div>
+      <div>
+      <img src="/svg/Obserwowane_inwestycje_ICON.svg" alt="plus" />
+        Obserwowane inwestycje</div>
+      <div>
+      <img src="/svg/ObserwowanePropozycjeFuzjaIcon.svg" alt="plus" />
+        Obserwowane propozycje</div>
     </div>
-    <div onClick={() => setVisible(visible => !visible)} className={`${styles.hideCircle} ${!visible?styles.hidden:""}`}></div>
+    <div onClick={() => setVisible(visible => !visible)} className={`${styles.hideCircle} ${!visible?styles.hidden:""}`}>
+
+      <img src="/svg/expand_less_FILL1_wght400_GRAD0_opsz24.svg" alt="plus" style={{display: visible?"block":"none"}} />
+      <img src="/svg/expand_more_FILL1_wght400_GRAD0_opsz24_2.svg" alt="plus" style={{display:visible?"none":"block"}} />
+    </div>
     </>
 
   );
